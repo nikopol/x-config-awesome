ce9a345b689c917f3b07ceb3e5d121735416482d
niko <niko@rtgi.fr>
2015-11-16 12:29:49 +0100

    default layouts updated

--- default/.config/awesome/rc.lua
+++ default/.config/awesome/rc.lua
@@ -69,12 +69,12 @@ local layouts = {
    -- awful.layout.suit.tile.left,
    awful.layout.suit.tile.bottom,
    -- awful.layout.suit.tile.top,
-   awful.layout.suit.fair,
+   -- awful.layout.suit.fair,
    -- awful.layout.suit.fair.horizontal,
    -- awful.layout.suit.spiral,
    awful.layout.suit.spiral.dwindle
    -- awful.layout.suit.max.fullscreen,
-   --awful.layout.suit.magnifier
+   -- awful.layout.suit.magnifier
 }
 -- }}}
 
